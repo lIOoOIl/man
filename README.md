7dedea0abd80be266cab412afdcc018b6d8fef07
pomelo1213 <goodmanluoxin@163.com>
2018-11-10 16:04:45 +0800

    update: 测试demo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # man
 # this is react-man
+
+## what i have learn from this project
+- [x] use create-react-app
+- [x] how to export webpack config from this project
+- [ ] how to use ant-design
+- [ ] how to wrapper my own ant-design
+- [ ] how to use react-router
---
+++ man/src/demo.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class Demo extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  plus = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  dele = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  render () {
+    return (
+      <div>
+        <h3>this is first test for react demo</h3>
+        <button onClick = { this.plus }> first </button>
+        <button onClick = { this.dele }> second </button>
+        <h3>{ this.state.count }</h3>
+      </div>
+    )
+  }
+}
+
+export default Demo
